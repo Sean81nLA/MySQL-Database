d1e53447e3ef501e89c36b77d726684e16b7ff57
Sean Karimov <47092283+Sean81nLA@users.noreply.github.com>
2021-11-27 19:01:34 -0800

    Time: 473 ms (64.97%), Space: 0B (100.00%) - LeetHub

---
+++ products-price-for-each-store/products-price-for-each-store.sql
@@ -0,0 +1,7 @@
+select product_id,
+sum(case when store="store1" Then price End) as store1,
+sum(case when store="store2" Then price End) as store2,
+sum(case when store="store3" Then price End)as store3
+from Products
+group by product_id
+
