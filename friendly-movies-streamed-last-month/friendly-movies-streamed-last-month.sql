98de6896fd58b53a347adc34c60beab7d1c0edb6
Sean Karimov <47092283+Sean81nLA@users.noreply.github.com>
2021-11-28 22:56:44 -0800

    Time: 1414 ms (5.07%), Space: 0B (100.00%) - LeetHub

---
+++ friendly-movies-streamed-last-month/friendly-movies-streamed-last-month.sql
@@ -0,0 +1,9 @@
+SELECT DISTINCT c.title AS title
+FROM TVProgram t
+LEFT JOIN Content c ON t.content_id = c.content_id
+WHERE 
+	YEAR(t.program_date) = 2020 
+	AND MONTH(t.program_date) = 6 
+	AND c.Kids_content = 'Y' 
+	AND c.content_type = 'Movies'
+;
