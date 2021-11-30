24105740c8aa0c7ba122db46d7961e6eee2ec92d
Sean Karimov <47092283+Sean81nLA@users.noreply.github.com>
2021-11-29 18:16:34 -0800

    Time: 282 ms (43.64%), Space: 0B (100.00%) - LeetHub

--- find-the-team-size/find-the-team-size.sql
+++ find-the-team-size/find-the-team-size.sql
@@ -1,8 +1,7 @@
-with team AS(
-Select team_id, count(employee_id) as 'team_size'
+With team AS(
+select team_id, count(employee_id) as'team_size'
 from employee
-Group By team_id)
+group by team_id)
 Select a.employee_id, b.team_size
-from employee a left join team b
+from employee a join team b
 on a.team_id=b.team_id
-
