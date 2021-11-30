e93ee0083702516f5aae4b5c975ef1de5b38cffc
Sean Karimov <47092283+Sean81nLA@users.noreply.github.com>
2021-11-30 06:58:48 -0800

    Time: 569 ms (45.35%), Space: 0B (100.00%) - LeetHub

--- warehouse-manager/warehouse-manager.sql
+++ warehouse-manager/warehouse-manager.sql
@@ -1,4 +1,4 @@
-select a.name as 'warehouse_name', sum(a.units*b.width*b.length*b.height) as volume
-from warehouse a join products b
-on a.product_id=b.product_id
+select a.name as "warehouse_name", sum(a.units*b.width*b.length*b.height) as "volume"
+from products b
+join warehouse a on a.product_id=b.product_id
 group by name
