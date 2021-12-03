6da97e0ad3039cc8adcf8a9aceacb5aad90cc6cc
Sean Karimov <47092283+Sean81nLA@users.noreply.github.com>
2021-12-03 12:02:03 -0800

    Time: 602 ms (22.59%), Space: 0B (100.00%) - LeetHub

--- rearrange-products-table/rearrange-products-table.sql
+++ rearrange-products-table/rearrange-products-table.sql
@@ -1,7 +1,6 @@
-select product_id, 'store1' as "store", store1 as price  from products where store1 is not NULL
-Union
-select product_id, 'store2' as "store", store2 as price 
-from products where store2 is not NULL
-Union
-select product_id, 'store3' as "store", store3 as price 
-from products where store3 is not NULL
+select product_id, 'store1' as 'store', store1 as price  from products where store1 is not null 
+union
+select product_id, 'store2' as 'store', store2 as price  from products where store2 is not null 
+union
+select product_id, 'store3' as 'store', store3 as price  from products where store3 is not null 
+
