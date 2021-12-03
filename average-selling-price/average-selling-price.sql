47937099d7ce781457b73dfdae1a6bc8b16c0aa3
Sean Karimov <47092283+Sean81nLA@users.noreply.github.com>
2021-12-03 12:31:18 -0800

    Time: 508 ms (95.34%), Space: 0B (100.00%) - LeetHub

--- average-selling-price/average-selling-price.sql
+++ average-selling-price/average-selling-price.sql
@@ -1,6 +1,7 @@
 select a.product_id,
-round(sum(units*price)/sum(units),2) as average_price
-from prices a join unitssold b on a.product_id=b.product_id
-where purchase_date between start_date and end_date
+round(sum(b.units*a.price)/sum(b.units),2) as average_price
+From prices a join UnitsSold b
+on a.product_id=b.product_id
+where b.purchase_date between start_date and end_date
 group by product_id
 
