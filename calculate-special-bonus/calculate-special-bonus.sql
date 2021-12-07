c9de4e03cc7a4fa753302812351062048b322e10
Sean Karimov <47092283+Sean81nLA@users.noreply.github.com>
2021-12-07 10:03:27 -0800

    Time: 540 ms (54.24%), Space: 0B (100.00%) - LeetHub

--- calculate-special-bonus/calculate-special-bonus.sql
+++ calculate-special-bonus/calculate-special-bonus.sql
@@ -1,3 +1,5 @@
-select employee_id,
-case when mod(employee_id,2)<>0 and name not like 'm%' Then salary Else 0 End as bonus
-from Employees
+With bonus AS(
+select employee_id, salary, name from Employees where name not like 'm%' and mod(employee_id,2)<>0)
+ select a.employee_id, ifnull(b.salary,0) as bonus
+ from employees a left join bonus b
+ on a.employee_id=b.employee_id
